a95a73e1a83f417319ddea37a10f015c52dc94e5
Dimas Begunoff <dbegunoff@gmail.com>
2010-12-10 09:39:15 -0800

    removed taxonomy related files

--- custom/custom_tax.php
+++
@@ -1,7 +0,0 @@
-<tr class="form-field">
-	<th scope="row" valign="top"><label><?php _e('Sample') ?></label></th>
-	<td>
-		<input type="text" name="sample" value="<?php if (isset($term->sample)) echo $term->sample; ?>"><br />
-		<span class="description">Sample data</span>
-	</td>
-</tr>

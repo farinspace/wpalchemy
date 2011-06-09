f3cdf90bbeae7a215456af19054576a86da0d868
Dimas Begunoff <dbegunoff@gmail.com>
2011-06-09 12:53:27 -0700

    get_the_value() now uses htmlentities() for values when populating form fields

--- WPAlchemy/MetaBox.php
+++ WPAlchemy/MetaBox.php
@@ -5,7 +5,7 @@
  * @copyright	Copyright (c) 2009, Dimas Begunoff, http://farinspace.com
  * @license		http://en.wikipedia.org/wiki/MIT_License The MIT License
  * @package		WPAlchemy
- * @version		1.4.9
+ * @version		1.4.10
  * @link		http://github.com/farinspace/wpalchemy
  * @link		http://farinspace.com
  */
@@ -1668,7 +1668,7 @@ class WPAlchemy_MetaBox
 
 		if ($this->in_template)
 		{
-			return $value;
+			return htmlentities($value, ENT_QUOTES);
 		}
 		else
 		{

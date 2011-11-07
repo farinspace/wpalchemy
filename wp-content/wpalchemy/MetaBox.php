ee0c38ca036c73dc5295c81beffce39d969f082d
Dimas Begunoff <dbegunoff@gmail.com>
2011-11-07 14:50:32 -0800

    version number update

--- wp-content/wpalchemy/MetaBox.php
+++ wp-content/wpalchemy/MetaBox.php
@@ -5,7 +5,7 @@
  * @copyright	Copyright (c) 2009, Dimas Begunoff, http://farinspace.com
  * @license		http://en.wikipedia.org/wiki/MIT_License The MIT License
  * @package		WPAlchemy
- * @version		1.4.15
+ * @version		1.4.16
  * @link		http://github.com/farinspace/wpalchemy
  * @link		http://farinspace.com
  */
